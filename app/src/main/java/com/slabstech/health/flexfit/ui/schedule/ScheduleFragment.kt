cb81838e2998cd012d5dc28ba261d08132e34389
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-12-02 20:44:32 +0100

    Change-header-values

--- app/src/main/java/com/slabstech/health/flexfit/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/slabstech/health/flexfit/ui/schedule/ScheduleFragment.kt
@@ -1,4 +1,4 @@
-package com.slabstech.health.flexfit.ui.gallery
+package com.slabstech.health.flexfit.ui.schedule
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.slabstech.health.flexfit.databinding.FragmentGalleryBinding
+import com.slabstech.health.flexfit.databinding.FragmentScheduleBinding
 
-class GalleryFragment : Fragment() {
+class ScheduleFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentScheduleBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+            ViewModelProvider(this).get(ScheduleViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentScheduleBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
--- app/src/main/java/com/slabstech/health/flexfit/ui/schedule/ScheduleViewModel.kt
+++ app/src/main/java/com/slabstech/health/flexfit/ui/schedule/ScheduleViewModel.kt
@@ -1,13 +1,13 @@
-package com.slabstech.health.flexfit.ui.gallery
+package com.slabstech.health.flexfit.ui.schedule
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class ScheduleViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Flex Fit Schedule - HIIT, Zumba, Weight Training"
     }
     val text: LiveData<String> = _text
 }
